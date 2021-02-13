b993fd5a731c7c642e07047713ee387f4c4e80ce
tNahoko <nahoko.toyota@gmail.com>
2021-02-13 09:27:20 +0900

    Remove and ignore node_modules

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+let corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Nahoko's app."});
+})
+
+const PORT = precess.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}.`);
+})
